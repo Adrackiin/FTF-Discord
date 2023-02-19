1cf534d657300132cfe266f74267fddd7d1a2cd6
Antoine <adrackiin@gmail.com>
2023-02-19 14:06:48 +0100

    Modification du message de mission.

--- src/events/reaction.ts
+++ src/events/reaction.ts
@@ -6,7 +6,7 @@ const database = ChallengeManager.getInstance();
 module.exports = {
     name: Events.MessageReactionAdd,
     async execute(reaction, user) {
-        if (reaction.message.id === "1075737220188753941") {
+        if (reaction.message.id === "1076852175193526272" && user.id != "481783592976908289") {
             if(!(await database.userExists(user.id))) {
                 await user.send("Vous avez accepté la mission ! Pour commencer l'infiltration, veuillez entrer la commande `/infiltration`.");
             }
